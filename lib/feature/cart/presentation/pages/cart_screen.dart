e0c02f5326fa72cf4d6327d36699be0cd40290a2
midhlaj-innsof <midhlaj@innsof.com>
2026-06-09 09:38:54 +0530

    Add Book Cart  feature with BLoC, models, and UI integration

--- lib/feature/cart/presentation/pages/cart_screen.dart
+++ lib/feature/cart/presentation/pages/cart_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:patient_portal/feature/cart/widgets/cart_item_tile.dart';
+import 'package:patient_portal/feature/cart/presentation/widgets/cart_item_tile.dart';
 import 'package:patient_portal/feature/lab/blocs/items_bloc/items_bloc.dart';
-
-import '../../resources/app_colors.dart';
-import '../../resources/app_text_styles.dart';
-import '../../resources/common_widgets.dart/common_appbar.dart';
+import 'package:patient_portal/resources/app_colors.dart';
+import 'package:patient_portal/resources/app_text_styles.dart';
+import 'package:patient_portal/resources/common_widgets.dart/common_appbar.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
--- lib/route/router.dart
+++ lib/route/router.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:patient_portal/feature/add_document/presentation/pages/add_document_screen.dart';
 import 'package:patient_portal/feature/add_member/presentation/pages/add_member_screen.dart';
 import 'package:patient_portal/feature/book_appointment/presentation/pages/book_appointment_page.dart';
-import 'package:patient_portal/feature/cart/cart_screen.dart';
+import 'package:patient_portal/feature/cart/presentation/pages/cart_screen.dart';
 import 'package:patient_portal/feature/doctor_detail/doctor_detail_screen.dart';
 import 'package:patient_portal/feature/documents/documents_screen.dart';
 import 'package:patient_portal/feature/edit_profile_details/edit_profile_details_screen.dart';
