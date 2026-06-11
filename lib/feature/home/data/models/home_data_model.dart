c9177e865c1f850685ea426560b032daef032c1a
midhlaj-innsof <midhlaj@innsof.com>
2026-06-11 17:24:02 +0530

    Map home departments to specialities

--- lib/feature/home/data/models/home_data_model.dart
+++ lib/feature/home/data/models/home_data_model.dart
@@ -38,7 +38,7 @@ Map<String, dynamic> _normalizeHomeDataJson(Map<String, dynamic> json) {
   return {
     ...json,
     'ad_banner': json['ad_banner'] ?? json['banner'] ?? const [],
-    'speciality': json['speciality'] ?? const [],
+    'speciality': json['speciality'] ?? json['department'] ?? const [],
     'insurance': json['insurance'] ?? const [],
     'package_banner': json['package_banner'] ?? const [],
     'doctors': json['doctors'] ?? const [],
