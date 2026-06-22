d30321e75f81c0642456e9aaf71b05ea392135a7
midhlaj-innsof <midhlaj@innsof.com>
2026-06-22 17:37:18 +0530

    Remove QuickMenuSection from ProfileScreen

--- lib/feature/profile/presentation/pages/profile_screen.dart
+++ lib/feature/profile/presentation/pages/profile_screen.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:patient_portal/feature/profile/presentation/widgets/member_section.dart';
 import 'package:patient_portal/feature/profile/presentation/widgets/my_profile_section.dart';
-import 'package:patient_portal/feature/profile/presentation/widgets/quick_menus_section.dart';
 
 @RoutePage(name: 'MyProfileRoute')
 class ProfileScreen extends StatelessWidget {
@@ -21,10 +20,10 @@ class ProfileScreen extends StatelessWidget {
             padding: EdgeInsets.fromLTRB(16, 16, 16, 12),
             sliver: SliverToBoxAdapter(child: ProfileDetailsSection()),
           ),
-          SliverPadding(
-            padding: EdgeInsets.fromLTRB(16, 0, 16, 12),
-            sliver: SliverToBoxAdapter(child: QuickMenuSection()),
-          ),
+          // SliverPadding(
+          //   padding: EdgeInsets.fromLTRB(16, 0, 16, 12),
+          //   sliver: SliverToBoxAdapter(child: QuickMenuSection()),
+          // ),
           SliverPadding(
             padding: EdgeInsets.fromLTRB(16, 0, 16, 112),
             sliver: SliverToBoxAdapter(child: MemberSection()),
