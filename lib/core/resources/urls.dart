c45a31dab5e6d99275bd76b66e0742fd8084cc93
midhlaj-innsof <midhlaj@innsof.com>
2026-06-25 10:51:02 +0530

    Update default API base URL

--- lib/core/resources/urls.dart
+++ lib/core/resources/urls.dart
@@ -2,8 +2,8 @@ class ConstantUrls {
   static const String cloudUrl = 'http://185.52.54.42:8081';
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://erp.innsof.com:8011',
-    //'http://192.168.1.24:8089',
+    defaultValue: 'http://192.168.1.24:8089',
+    //'http://erp.innsof.com:8011',
     //  defaultValue: 'http://192.168.1.13:5237',
   );
   static const String serviceUrl = '$baseUrl/api/Apm/service';
