699556eba072b616a6135de29d0c9725627b867d
midhlaj-innsof <midhlaj@innsof.com>
2026-06-25 14:40:45 +0530

    Temporarily disable notification icon in HomeAppBar
    * Comment out the notification button in `HomeAppBar` and add a TODO for its completion.
    * Remove unused imports related to routing, assets, and the notification icon button.

--- lib/feature/main_screen/presentation/widgets/home_appbar.dart
+++ lib/feature/main_screen/presentation/widgets/home_appbar.dart
@@ -1,11 +1,7 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:patient_portal/core/gen/assets.gen.dart';
-import 'package:patient_portal/core/route/app_router.dart';
 import 'package:patient_portal/feature/home/presentation/bloc/home_bloc/home_bloc.dart';
 import 'package:patient_portal/feature/main_screen/presentation/widgets/main_shell_app_bar.dart';
-import 'package:patient_portal/feature/main_screen/presentation/widgets/main_shell_icon_button.dart';
 
 class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   const HomeAppBar({super.key});
@@ -17,14 +13,15 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
         return MainShellAppBar(
           titleWidget: const MainShellLogoTitle(),
           actions: [
-            MainShellIconButton(
-              iconPath: Assets.icons.notificationIcon.path,
-              tooltip: 'Notifications',
-              badgeLabel: '${state.homeData.notificationCount}',
-              onPressed: () {
-                context.router.push(const NotificationRoute());
-              },
-            ),
+            //TODO ::: add notification icon and complete
+            // MainShellIconButton(
+            //   iconPath: Assets.icons.notificationIcon.path,
+            //   tooltip: 'Notifications',
+            //   badgeLabel: '${state.homeData.notificationCount}',
+            //   onPressed: () {
+            //     context.router.push(const NotificationRoute());
+            //   },
+            // ),
           ],
         );
       },
